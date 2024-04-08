b5eaa2bf72ad676e46af2ff24504fed5176270d6
Vika2335 <98107387+Vika2335@users.noreply.github.com>
2024-04-08 16:31:52 +0300

    refactor: style css

--- src/page/MainPage/MainPage.jsx
+++ src/page/MainPage/MainPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './MainPage.css'
 import { useGetPostsQuery } from '../../redux/postsApi'
 import { Link } from 'react-router-dom'
+import comment from '../../image/comment.svg';
 
 function MainPage() {
   const { data: posts, isLoading } = useGetPostsQuery();
@@ -11,10 +12,19 @@ function MainPage() {
   const contentPosts = posts.map((item) => {
     return(
       <div className='post' key={item._id}>
-        <Link to={`/post/${item._id}`}>
+        <Link className='link' to={`/post/${item._id}`}>
           <div className='post__button'>
             <h2 className='title'>{item.header}</h2>
             <p className='description'>{item.body}</p>
+            <p className='tags'>{item.tags}</p>
+            <div className='post__content'>
+              <div className='views'>
+                <p className='int'>2.2K</p>
+              </div>
+              <div className='comment'>
+                <p className='int'>10</p>
+              </div>
+            </div>
           </div>
         </Link>
       </div>
