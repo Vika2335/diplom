7510271cd21006098c634f4c750478bdbe67ba32
Vika2335 <golobokova.viktoriakpc602020@gmail.com>
2024-04-12 17:17:25 +0300

    feat: pagination, footer fixed, img information, filter

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,27 +1,30 @@
 import React from 'react'
 import './Footer.css'
+import { useLocation } from 'react-router-dom'
 
 function Footer() {
-    return (
-      <>
-        <footer id="footer">
-          <div className="container">
-            <div className="footer__content">
-              <div className="content">
-                <div className='connect'>
-                  <p className='phone'>Телефон: 8(9ХХ)-ХХХ-ХХ ХХ</p>
-                  <p className='email'>Почта: Discurs@mail.ru</p>
-                </div>
-                <div>
-                  <p className='information'>Сайт является результатом дипломной работы. Не является коммерческим проектом</p>
-                </div>
+  const location = useLocation();
+
+  return (
+    <>
+      <footer id="footer" className={ `footer ${ location.pathname === '/' ? 'footer-main' : 'footer-autorization' }` }>
+        <div className="container">
+          <div className="footer__content">
+            <div className="content">
+              <div className='connect'>
+                <p className='phone'>Телефон: 8(9ХХ)-ХХХ-ХХ ХХ</p>
+                <p className='email'>Почта: Discurs@mail.ru</p>
               </div>
-              <h2 className='city'>Кострома, 2024г.</h2>
+              <div>
+                <p className='information'>Сайт является результатом дипломной работы. Не является коммерческим проектом</p>
+              </div>
             </div>
+            <h2 className='city'>Кострома, 2024г.</h2>
           </div>
-        </footer>
-      </>
-    )
-  }
-  
-  export default Footer;
+        </div>
+      </footer>
+    </>
+  )
+}
+
+export default Footer;
--- src/page/MainPage/MainPage.jsx
+++ src/page/MainPage/MainPage.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './MainPage.css'
 import { useGetPostsQuery } from '../../redux/postsApi'
 import { Link } from 'react-router-dom'
-import comment from '../../image/comment.svg';
+import comment from '../../image/icons/comment.svg';
+import eye from '../../image/icons/eye.svg'
+import heart from '../../image/icons/heart.svg'
+import datetime from '../../image/icons/datetime.svg'
+import lupa from '../../image/icons/lupa.svg'
+import { format } from 'date-fns';
 
 function MainPage() {
   const { data: posts, isLoading } = useGetPostsQuery();
-  
-  if (isLoading) return <h1>Loading...</h1>
 
-  const contentPosts = posts.map((item) => {
+  const [headerFilter, setHeaderFilter] = useState('');
+  const [bodyFilter, setBodytFilter] = useState('');
+  const [tagFilter, setTagFilter] = useState('');
+  const [filteredPosts, setFilteredPosts] = useState([]);
+
+  useEffect(() => {
+    if (!isLoading) {
+      setFilteredPosts(posts || []);
+    }
+  }, [isLoading, posts]);
+
+  const handleSearch = () => {
+    const filteredPosts = posts.filter((post) => {
+      const tags = post.tags && typeof post.tags === 'string' ? post.tags : '';
+      const headerMatch = headerFilter.trim() === '' || post.header.toLowerCase().includes(headerFilter.toLowerCase());
+      const bodyMatch = bodyFilter.trim() === '' || post.body.toLowerCase().includes(bodyFilter.toLowerCase());
+      const tagMatch = tagFilter.trim() === '' || tags.toLowerCase().includes(tagFilter.toLowerCase());
+      return headerMatch && bodyMatch && tagMatch;
+    });
+    setFilteredPosts(filteredPosts);
+  };
+
+  const contentPosts = filteredPosts.map((item) => {
     return(
       <div className='post' key={item._id}>
         <Link className='link' to={`/post/${item._id}`}>
@@ -17,12 +42,24 @@ function MainPage() {
             <h2 className='title'>{item.header}</h2>
             <p className='description'>{item.body}</p>
             <p className='tags'>{item.tags}</p>
-            <div className='post__content'>
-              <div className='views'>
-                <p className='int'>2.2K</p>
+            <div className='information__post'>
+              <div className='post__content'>
+                <div className='views'>
+                  <p className='view__int'>2.2K</p>
+                  <img src={eye} alt='No image'/>
+                </div>
+                <div className='comment'>
+                  <p className='int'>10</p>
+                  <img src={comment} alt='No image'/>
+                </div>
+                <div className='hearts'>
+                  <p className='int'>5</p>
+                  <img src={heart} alt='No image'/>
+                </div>
               </div>
-              <div className='comment'>
-                <p className='int'>10</p>
+              <div className='datetime'>
+                <p className='date'>{format(new Date(item.createdAt), 'dd.MM.yyyy')}</p>
+                <img src={datetime} alt='No image'/>
               </div>
             </div>
           </div>
@@ -31,13 +68,50 @@ function MainPage() {
     )
   }) 
 
+  if (isLoading) return <h1 className='load'>Loading...</h1>
+
   return (
     <>
       <main>
         <section className='main'>
           <div className="container">
+            <div className='filters'>
+              <div className='filter'>
+                <input className='filter__name' type='text' placeholder='Поиск по названию...' value={headerFilter} onChange={(e) => setHeaderFilter(e.target.value)}/>
+                <button className='button__lupa' onClick={handleSearch}><img className='lupa' src={lupa} alt='No image'/></button>
+              </div>
+              <div className='filter'>
+                <input className='filter__content' type='text' placeholder='Поиск по содержанию...' value={bodyFilter} onChange={(e) => setBodytFilter(e.target.value)}/>
+                <button className='button__lupa' onClick={handleSearch}><img className='lupa' src={lupa} alt='No image'/></button>
+              </div>
+              <div className='filter'>
+                <input className='filter__tag' type='text' placeholder='Поиск по тегу...' value={tagFilter} onChange={(e) => setTagFilter(e.target.value)}/>
+                <button className='button__lupa' onClick={handleSearch}><img className='lupa' src={lupa} alt='No image'/></button>
+              </div>
+            </div>
+            <div className='buttons'>
+              <div className='button__new'><button className='new'>Новое</button></div>
+              <div className='button__popular'><button className='popular'>Популярное</button></div>
+            </div>
             <div className="posts__gallery">
               {contentPosts}
+            </div>
+            <div className="nav">
+              <div className="pagination">
+                <button className="pagination__button-img" id="firstback">
+                  &lt;&lt;
+                </button>
+                <button className="pagination__button-img" id="back">
+                  &lt;
+                </button>
+                <button className="pagination__button-img pagination__button" id="page"></button>
+                <button className="pagination__button-img nav-img" id="forward">
+                  &gt;
+                </button>
+                <button className="pagination__button-img nav-img" id="lastforward">
+                  &gt;&gt;
+                </button>
+              </div>
             </div>
           </div>
         </section>
